100edf8756d3a2f3c59f1a346318d2cda003eda7
Francesco Marangi <f.marangi@mzentrale.de>
2017-12-15 00:57:03 +0100

    Day 10.2

--- spec/Day10Spec.php
+++ spec/Day10Spec.php
@@ -24,4 +24,19 @@ class Day10Spec extends ObjectBehavior
     {
         $this->factor([3, 4, 1, 5], 5)->shouldReturn(12);
     }
+
+    function it_should_hash_strings()
+    {
+        $this->hash('')->shouldReturn('a2582a3a0e66e6e86e3812dcb672a272');
+        $this->hash('AoC 2017')->shouldReturn('33efeb34ea91902bb2f59c9920caa6cd');
+        $this->hash('1,2,4')->shouldReturn('63960835bcdc130f0b66d7ff4f6a5a8e');
+        $this->hash('1,2,3')->shouldReturn('3efbe78a8d82f29979031a4aa0b16a9d');
+    }
+
+    public function it_should_solve_the_puzzle()
+    {
+        $input = file_get_contents(dirname(__DIR__) . '/input/day10.txt');
+        $this->part1($input)->shouldReturn(8536);
+        $this->part2($input)->shouldReturn('aff593797989d665349efe11bb4fd99b');
+    }
 }
--- src/Day10.php
+++ src/Day10.php
@@ -4,9 +4,14 @@ namespace Mzentrale\AdventOfCode;
 
 class Day10 implements Puzzle
 {
-    public function solve($input)
+    public function part1(string $input)
     {
         return $this->factor(explode(',', trim($input)));
+    }
+
+    public function part2(string $input)
+    {
+        return $this->hash(trim($input));
     }
 
     public function factor(array $lengths, int $numItems = 256): int
@@ -34,5 +39,34 @@ class Day10 implements Puzzle
             $list[($i + $position) % $count] = $number;
         }
         return $list;
+    }
+
+    public function hash(string $input): string
+    {
+        $dense = array_map(function ($numbers) {
+            return array_reduce($numbers, function ($result, $number) {
+                return $result ^ $number;
+            }, 0);
+        }, $this->getSparseHash($this->getLengths($input)));
+        return implode('', array_map(function ($num) {
+            return sprintf('%02x', $num);
+        }, $dense));
+    }
+
+    private function getLengths(string $input): array
+    {
+        return array_merge(array_filter(array_map('ord', str_split($input))), [17, 31, 73, 47, 23]);
+    }
+
+    private function getSparseHash(array $lengths, int $numItems = 256, int $repeat = 64): array
+    {
+        $list = range(0, $numItems - 1);
+        for ($i = 0, $position = 0, $skip = 0; $i < $repeat; $i++) {
+            foreach ($lengths as $length) {
+                $list     = $this->apply($list, $position, $length);
+                $position += ($length + $skip++) % $numItems;
+            }
+        }
+        return array_chunk($list, 16);
     }
 }
