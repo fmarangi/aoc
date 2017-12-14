d0cf6195b4f09edcb84e6a64f1dd61318355f96a
Francesco Marangi <f.marangi@mzentrale.de>
2017-12-14 16:32:50 +0100

    Day 7.2

--- spec/Day7Spec.php
+++ spec/Day7Spec.php
@@ -14,13 +14,31 @@ class Day7Spec extends ObjectBehavior
 
     function it_should_parse_lines()
     {
-        $this->parse('tknk (41) -> ugml, padx, fwft')->shouldReturn(['tknk', ['ugml', 'padx', 'fwft']]);
-        $this->parse('pbga (66)')->shouldReturn(['pbga', []]);
+        $this->parse('tknk (41) -> ugml, padx, fwft')->shouldContain('tknk');
+        $this->parse('pbga (66)')->shouldReturn(['pbga', [], 66]);
+        $this->parse('pbga (66)')->shouldContain('pbga');
     }
 
-    function it_should_solve_the_problem()
+    function it_should_find_the_bottom_tower()
     {
-        $this->solve("pbga (66)
+        $this->findBottom($this->input())->shouldReturn('tknk');
+    }
+
+    function it_should_find_the_wrong_weight()
+    {
+        $this->findWrong($this->input())->shouldReturn(60);
+    }
+
+    public function it_should_solve_the_puzzle()
+    {
+        $input = file_get_contents(dirname(__DIR__) . '/input/day7.txt');
+        $this->part1($input)->shouldReturn('bpvhwhh');
+        $this->part2($input)->shouldReturn(256);
+    }
+
+    private function input(): string
+    {
+        return 'pbga (66)
 xhth (57)
 ebii (61)
 havc (66)
@@ -33,6 +51,6 @@ jptl (61)
 ugml (68) -> gyxo, ebii, jptl
 gyxo (61)
 cntj (57)
-")->shouldReturn('tknk');
+';
     }
 }
