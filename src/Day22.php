a75c57394605ea0c562ef56e0438a6564fc771d2
Francesco Marangi <f.marangi@mzentrale.de>
2018-11-09 09:10:46 +0100

    Day 22

--- src/Day22.php
+++ src/Day22.php
@@ -9,6 +9,11 @@ class Day22 implements Puzzle
     const STATE_INFECTED = 2;
     const STATE_FLAGGED  = 3;
 
+    const DIRECTION_UP    = 0;
+    const DIRECTION_RIGHT = 1;
+    const DIRECTION_DOWN  = 2;
+    const DIRECTION_LEFT  = 3;
+
     public function part1(string $input)
     {
         return $this->work($input, 10000);
@@ -16,6 +21,7 @@ class Day22 implements Puzzle
 
     public function part2(string $input)
     {
+        return $this->work2($input, 10000000);
     }
 
     public function parseInput(string $grid): array
@@ -30,37 +36,76 @@ class Day22 implements Puzzle
         return $result;
     }
 
+    private function parseInput2(string $grid): array
+    {
+        $result = [];
+        $center = floor(strpos($grid, "\n") / 2);
+        foreach (explode("\n", trim($grid)) as $j => $row) {
+            foreach (str_split($row) as $k => $cell) {
+                $result[$j - $center][$k - $center] = $cell === '#' ? self::STATE_INFECTED : self::STATE_CLEAN;
+            }
+        }
+        return $result;
+    }
+
+
     public function work(string $gridData, int $iterations): int
     {
         $grid       = $this->parseInput($gridData);
         $position   = [0, 0];
-        $direction  = 'u';
+        $direction  = self::DIRECTION_UP;
         $infections = 0;
         for ($i = 0; $i < $iterations; $i++) {
             $isInfected                       = $grid[$position[0]][$position[1]] ?? false;
             $grid[$position[0]][$position[1]] = !$isInfected;
             $infections                       += !$isInfected;
-            switch ($direction) {
-                case 'u':
-                    $direction = $isInfected ? 'r' : 'l';
-                    $position  = [$position[0], $position[1] + ($isInfected ? 1 : -1)];
-                    break;
-                case 'd':
-                    $direction = $isInfected ? 'l' : 'r';
-                    $position  = [$position[0], $position[1] + (!$isInfected ? 1 : -1)];
-                    break;
-                case 'l':
-                    $direction = $isInfected ? 'u' : 'd';
-                    $position  = [$position[0] + ($isInfected ? -1 : 1), $position[1]];
-                    break;
-                case 'r':
-                    $direction = $isInfected ? 'd' : 'u';
-                    $position  = [$position[0] + (!$isInfected ? -1 : 1), $position[1]];
-                    break;
-            }
+
+            $direction = $this->getNewDirection($direction, $isInfected ? 1 : -1);
+            $position  = $this->getNewPosition($position, $direction);
         }
 
         return $infections;
+    }
+
+    public function work2(string $gridData, int $iterations): int
+    {
+        $grid       = $this->parseInput2($gridData);
+        $position   = [0, 0];
+        $direction  = self::DIRECTION_UP;
+        $infections = 0;
+        $move       = [
+            self::STATE_CLEAN    => -1,
+            self::STATE_INFECTED => 1,
+            self::STATE_FLAGGED  => 2,
+        ];
+        for ($i = 0; $i < $iterations; $i++) {
+            $state                            = $grid[$position[0]][$position[1]] ?? self::STATE_CLEAN;
+            $newState                         = ($state + 1) % 4;
+            $grid[$position[0]][$position[1]] = $newState;
+            $infections                       += $newState === self::STATE_INFECTED;
+
+            $direction = $this->getNewDirection($direction, $move[$state] ?? 0);
+            $position  = $this->getNewPosition($position, $direction);
+        }
+
+        return $infections;
+    }
+
+    private function getNewDirection(int $current, int $move): int
+    {
+        return ($current + 4 + $move) % 4;
+    }
+
+    private function getNewPosition(array $current, int $direction): array
+    {
+        switch ($direction) {
+            case self::DIRECTION_UP:
+            case self::DIRECTION_DOWN:
+                return [$current[0] + ($direction === self::DIRECTION_UP ? -1 : 1), $current[1]];
+            case self::DIRECTION_RIGHT:
+            case self::DIRECTION_LEFT:
+                return [$current[0], $current[1] + ($direction === self::DIRECTION_LEFT ? -1 : 1)];
+        }
     }
 
     private function debug(array $grid)
