14e87ed50ac335f6132dca10d556a141742d2597
Francesco Marangi <f.marangi@mzentrale.de>
2018-11-11 14:35:09 +0100

    Day 23.1

---
+++ spec/Day23Spec.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace spec\Mzentrale\AdventOfCode;
+
+use Mzentrale\AdventOfCode\Puzzle;
+use PhpSpec\ObjectBehavior;
+
+class Day23Spec extends ObjectBehavior
+{
+    function it_is_a_puzzle()
+    {
+        $this->shouldHaveType(Puzzle::class);
+    }
+
+    function it_solves_the_puzzle()
+    {
+        $input = file_get_contents(dirname(__DIR__) . '/input/day23.txt');
+        $this->part1($input)->shouldReturn(8281);
+    }
+}
