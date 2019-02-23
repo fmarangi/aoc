09f2d4b3d146426d261a429a696accdee191a3d8
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-02-23 11:48:23 +0100

    Day 2016.22

--- spec/Year2016/Day22Spec.php
+++ spec/Year2016/Day22Spec.php
@@ -7,14 +7,32 @@ use PhpSpec\ObjectBehavior;
 
 class Day22Spec extends ObjectBehavior
 {
+    /** @var string */
+    private $sample = 'Filesystem            Size  Used  Avail  Use%
+/dev/grid/node-x0-y0   10T    8T     2T   80%
+/dev/grid/node-x0-y1   11T    6T     5T   54%
+/dev/grid/node-x0-y2   32T   28T     4T   87%
+/dev/grid/node-x1-y0    9T    7T     2T   77%
+/dev/grid/node-x1-y1    8T    0T     8T    0%
+/dev/grid/node-x1-y2   11T    7T     4T   63%
+/dev/grid/node-x2-y0   10T    6T     4T   60%
+/dev/grid/node-x2-y1    9T    8T     1T   88%
+/dev/grid/node-x2-y2    9T    6T     3T   66%';
+
     function it_is_initializable()
     {
         $this->shouldHaveType(Puzzle::class);
+    }
+
+    function it_gains_access_to_data()
+    {
+        $this->gainDataAccess($this->sample)->shouldReturn(7);
     }
 
     function it_solves_the_puzzle()
     {
         $input = file_get_contents(dirname(dirname(__DIR__)) . '/input/year2016/day22.txt');
         $this->part1($input)->shouldReturn(1034);
+        $this->part2($input)->shouldReturn(261);
     }
 }
--- src/Year2016/Day22.php
+++ src/Year2016/Day22.php
@@ -6,8 +6,9 @@ use Mzentrale\AdventOfCode\Puzzle;
 
 class Day22 implements Puzzle
 {
-    const USED  = 3;
-    const AVAIL = 4;
+    private const USED  = 2;
+    private const AVAIL = 3;
+    private const MOVES = 5;
 
     public function part1(string $input)
     {
@@ -19,7 +20,82 @@ class Day22 implements Puzzle
 
     public function part2(string $input)
     {
-        // TODO: Implement part2() method.
+        return $this->gainDataAccess($input);
+    }
+
+    public function gainDataAccess(string $input): int
+    {
+        $nodes = $this->parseInput($input);
+        $grid  = $this->getGrid($nodes);
+        $from  = [0, max(array_column($nodes, 0))];
+        $dist  = $from[1] - 1;
+        return $this->findShortestPath($this->toMaze($grid)) + $dist * self::MOVES;
+    }
+
+    private function findShortestPath(string $maze): int
+    {
+        $goal = strpos($maze, 'G');
+        $from = strpos($maze, '_');
+        $max  = strlen($maze) - 1;
+        $next = $this->next($maze);
+
+        $seen  = [$from];
+        $queue = [[0, $from]];
+        while ($queue) {
+            [$steps, $p] = array_shift($queue);
+            foreach ($next($p) as $n) {
+                if ($n === $goal) return $steps + 1;
+                if ($n > $max || in_array($n, $seen) || $maze{$n} !== '.') continue;
+                $seen[]  = $n;
+                $queue[] = [$steps + 1, $n];
+            }
+        }
+        return -1;
+    }
+
+    private function next(string $maze): callable
+    {
+        $width = strpos($maze, PHP_EOL) + 1;
+        return function (int $point) use ($width): array {
+            return [$point - 1, $point + 1, $point - $width, $point + $width];
+        };
+    }
+
+    private function toMaze(array $grid): string
+    {
+        for ($y = 0, $maze = '', $maxY = count($grid); $y < $maxY; $y++) {
+            for ($x = 0, $maxX = count($grid[$y]); $x < $maxX; $x++) {
+                $offsets = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+                switch (true) {
+                    case $grid[$y][$x][0] === 0:
+                        $maze .= '_';
+                        break;
+                    case $y === 0 && $x === $maxX - 1:
+                        $maze .= 'G';
+                        break;
+                    default:
+                        foreach ($offsets as $o) {
+                            [$x_, $y_] = [$x + $o[0], $y + $o[1]];
+                            if (isset($grid[$y_][$x_]) && array_sum($grid[$y_][$x_]) < $grid[$y][$x][0]) {
+                                $maze .= '#';
+                                continue 2;
+                            }
+                        }
+                        $maze .= '.';
+                        break;
+                }
+            }
+            $maze .= PHP_EOL;
+        }
+        return $maze;
+    }
+
+    private function getGrid(array $nodes): array
+    {
+        for ($grid = [], $i = 0, $max = count($nodes); $i < $max; $i++) {
+            $grid[$nodes[$i][1]][$nodes[$i][0]] = array_slice($nodes[$i], 2);
+        }
+        return $grid;
     }
 
     private function viable(array $node): callable
@@ -32,7 +108,7 @@ class Day22 implements Puzzle
     private function parseInput(string $input): array
     {
         return array_values(array_filter(array_map(function (string $line): array {
-            preg_match('#/dev/grid/node-x(\d+)-y(\d+)\s+(\d+)T\s+(\d+)T\s+(\d+)T#', $line, $match);
+            preg_match('#^/dev/grid/node-x(\d+)-y(\d+)\s+\d+T\s+(\d+)T\s+(\d+)T#', $line, $match);
             return array_map('intval', array_slice($match, 1));
         }, explode(PHP_EOL, trim($input)))));
     }
