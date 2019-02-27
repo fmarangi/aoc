e2b4e71e2d89e2b00925c88d3de022f22832d9d2
Francesco Marangi <2734054+fmarangi@users.noreply.github.com>
2019-02-27 21:07:33 +0100

    Day 2018.23.2

--- spec/Year2018/Day23Spec.php
+++ spec/Year2018/Day23Spec.php
@@ -26,9 +26,20 @@ pos=<1,3,1>, r=1';
         $this->getInRange($input)->shouldReturn(7);
     }
 
+    function it_calculates_the_point_with_the_largest_number_of_bots()
+    {
+        $this->getMostInRange('pos=<10,12,12>, r=2
+pos=<12,14,12>, r=2
+pos=<16,12,12>, r=4
+pos=<14,14,14>, r=6
+pos=<50,50,50>, r=200
+pos=<10,10,10>, r=5')->shouldReturn(36);
+    }
+
     function it_solves_the_puzzle()
     {
         $input = file_get_contents(dirname(dirname(__DIR__)) . '/input/year2018/day23.txt');
         $this->part1($input)->shouldReturn(420);
+        $this->part2($input)->shouldReturn(124276103);
     }
 }
--- src/Year2018/Day23.php
+++ src/Year2018/Day23.php
@@ -13,7 +13,7 @@ class Day23 implements Puzzle
 
     public function part2(string $input)
     {
-        // TODO: Implement part2() method.
+        return $this->getMostInRange($input);
     }
 
     public function getInRange(string $input): int
@@ -25,6 +25,42 @@ class Day23 implements Puzzle
         return count($inRange);
     }
 
+    public function getMostInRange(string $input): int
+    {
+        $nanobots = $this->parseInput($input);
+        $inRange  = $this->ranges($nanobots);
+        $largest  = $this->getLargestClique($inRange);
+        return max(array_map(function (int $b) use ($nanobots): int {
+            return $this->minDistance($nanobots[$b]);
+        }, $largest));
+    }
+
+    private function getLargestClique(array $ranges): array
+    {
+        $count = array_map('count', $ranges);
+        $freq  = max(array_count_values($count));
+        $qty   = array_search($freq, $count);
+        return $ranges[array_search($qty, $count)];
+    }
+
+    private function ranges(array $nanobots): array
+    {
+        $num    = count($nanobots);
+        $ranges = array_map(function (int $nanobot): array {
+            return [$nanobot];
+        }, array_keys($nanobots));
+
+        for ($j = 0; $j < $num - 1; $j++) {
+            for ($k = $j + 1; $k < $num; $k++) {
+                if ($this->bothInRange($nanobots[$j], $nanobots[$k])) {
+                    array_push($ranges[$j], $k);
+                    array_push($ranges[$k], $j);
+                }
+            }
+        }
+        return $ranges;
+    }
+
     private function inRange(array $nanobot): callable
     {
         return function (array $other) use ($nanobot): bool {
@@ -32,9 +68,19 @@ class Day23 implements Puzzle
         };
     }
 
+    private function bothInRange(array $a, array $b): bool
+    {
+        return $this->distance($a, $b) <= $a[3] + $b[3];
+    }
+
     private function distance(array $a, array $b): int
     {
         return abs($a[0] - $b[0]) + abs($a[1] - $b[1]) + abs($a[2] - $b[2]);
+    }
+
+    private function minDistance(array $nanobot, array $to = [0, 0, 0]): int
+    {
+        return $this->distance($nanobot, $to) - $nanobot[3];
     }
 
     private function parseInput(string $input): array
